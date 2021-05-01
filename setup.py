e3c6cd4c5ed57f891f9929c031b2f4dca03339ed
thegismar <thegismar@gmail.com>
2021-05-01 06:04:30 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
+import setuptools
 from distutils.core import setup
 
 setup(name='py-chainscan',
       version='0.1.0',
-      packages=['chainscan']
+      packages=['chainscan'],
       setup_requires=['wheel']  
      )
